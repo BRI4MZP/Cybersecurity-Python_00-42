8243c366d9610a6cc6900d1dd02e33f83e1965ec
BRI4MZP <riveirodelrio@gmail.com>
2023-06-06 20:01:21 +0200

    100%

--- ex10/loading.py
+++ ex10/loading.py
@@ -1,4 +1,29 @@
-listy = range(1000) ret = 0
-for elem in ft_progress(listy): ret += (elem + 3) % 5
-sleep(0.01) print()
-print(ret)
+import sys
+import time
+
+def ft_progress(lst):
+    lensize = len(lst)
+    start_time = time.time()
+    etatime = 0
+    for i, index in enumerate(lst):
+        elapsed_time = time.time() - start_time
+        percent_complete = (i + 1) * 100 / lensize
+        filled_slots = int (percent_complete / 5)
+        empty_slots = 20 - filled_slots
+        etatime = (elapsed_time / (percent_complete / 100.0))
+        
+        progress = "[" + "=" * filled_slots + ">" + " " * empty_slots + "]"
+
+        sys.stdout.write("\r ETA: {:.2f}s [{:0.2f}%] {} ({}/{}) | elapsed time {:.2f}s".format(etatime, percent_complete, progress, i+1, lensize, elapsed_time))
+        sys.stdout.flush()
+        yield index
+
+if __name__ == "__main__":
+    lst = range(100)
+    retu = 0
+    for element in ft_progress(lst):
+        retu += element
+        time.sleep(0.005)
+    print()
+    print("...")
+    print(retu)
